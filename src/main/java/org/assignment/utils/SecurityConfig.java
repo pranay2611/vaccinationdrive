dc2f2b6619b1190f49f3bdfc6337f6eb867ef110
pranay2611 <pranaybiswas2611@gmail.com>
2025-05-04 14:26:33 +0530

    swagger fixes

--- src/main/java/org/assignment/utils/SecurityConfig.java
+++ src/main/java/org/assignment/utils/SecurityConfig.java
@@ -20,18 +20,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .cors(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
-        http.authorizeHttpRequests(
-                c -> c
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**", "/v2/api-docs/**", "/api/**", "/api/dashboard/**").permitAll() // Allow access to Swagger UI
-                        .anyRequest().authenticated()
-        );
-        http.csrf(
-                c -> c.disable()
-        );
-
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
 }
